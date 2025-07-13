12c44a568b055dcf76d6ec29901554ca269c9a5c
Makgomo-77 <102011950+Makgomo-77@users.noreply.github.com>
2025-07-13 20:39:29 +0800

    Update 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -1,13 +1,10 @@
-const sizeArg = process.argv[2];
-const size = parseInt(sizeArg);
+
+const size = parseInt(process.argv[2]);
 if(isNaN(size)){
     console.log("Missing size");
 }else{
     for(let i = 0; i < size; i++){
-        let row = '';
-        for(let j = 0; j < size; j++){
-            row += 'X';
+        console.log('x'.repeat(size))
         }
-        console.log(row);
-    }
-}
+        
+    
