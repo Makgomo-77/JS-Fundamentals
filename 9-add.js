75e61ca2b3ebe690d29aae99685b8bd301d2d667
Makgomo-77 <102011950+Makgomo-77@users.noreply.github.com>
2025-07-13 21:42:29 +0800

    Update 9-add.js

--- 9-add.js
+++ 9-add.js
@@ -1,4 +1,11 @@
 function add(a, b) {
-    const sum= a + b;
-    console.log(`The sum of ${a} and ${b} is: ${sum}`);
+    return a + b;
+}
+const num1= parseInt(process.argv[2]);
+const num2= parseInt(process.argv[3]);
+
+if (isNaN(num1) || isNaN(num2)){
+    console.log(NaN);
+}else{
+    console.log(add(num1, num2));
 }
