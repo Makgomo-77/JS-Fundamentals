e07c6d60c12219b8648bcfa51fc018d29e542b43
Makgomo-77 <102011950+Makgomo-77@users.noreply.github.com>
2025-07-13 21:55:44 +0800

    Update 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -5,7 +5,11 @@ if (isNaN(size)) {
     console.log("Missing size");
 }else{
     for (let i = 0; i < size; i++) {
-        console.log('x'.repeat(size))
+        let row ='';
+     for (let j =0; j <size; j++) {
+         row += 'x';
+     }
+        console.log(row);
         }
-        
+} 
     
