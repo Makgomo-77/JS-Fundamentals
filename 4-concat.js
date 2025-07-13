b7804c86ce57a6ce0840882294c682f8fd928d32
Makgomo-77 <102011950+Makgomo-77@users.noreply.github.com>
2025-07-13 19:32:53 +0800

    Update 4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,6 +1,6 @@
 function printArguments(arg1, arg2){
 console.log(`Argument 1: ${arg1} is ${arg2}`);
 }
-printArguments("python is fun")
-printArguments("undefined is undefined")
-printArguments("HBDN is undefined")
+printArguments("python fun");
+printArguments("undefined");
+printArguments("HBDN ");
