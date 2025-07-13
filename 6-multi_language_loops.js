4227f9d4c98d6c47a3747e3d187aa0ce17a8d793
Makgomo-77 <102011950+Makgomo-77@users.noreply.github.com>
2025-07-13 20:21:07 +0800

    Update 6-multi_language_loops.js

--- 6-multi_language_loops.js
+++ 6-multi_language_loops.js
@@ -1,4 +1,6 @@
-const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for(const line of languages){
-    console.log(line);
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let output= "";
+for(let i=0; i<lines.length; i++){
+    output +=lines[i] + (i <lines.length - 1? '\n':'');
 }
+console.log(output);
