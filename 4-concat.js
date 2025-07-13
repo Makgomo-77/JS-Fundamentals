ff4fbc6b45aff2ea7cd118d6a354dcfede2787ee
Makgomo-77 <102011950+Makgomo-77@users.noreply.github.com>
2025-07-13 19:19:03 +0800

    Update 4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,3 +1,3 @@
-const arg1= Process.argv[2];
-const arg2= Process.argv[3];
-console.log(`Argument 1: ${arg1}~ is ${arg2}`);
+function printArguments(arg1, arg2){
+console.log(`Argument 1: ${arg1} is ${arg2}`);
+}
