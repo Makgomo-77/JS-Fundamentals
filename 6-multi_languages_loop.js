ad0f517c8f1a8a12585309f7830cd45b489c0afa
Makgomo-77 <102011950+Makgomo-77@users.noreply.github.com>
2025-07-13 21:19:27 +0800

    Update 6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,4 @@
-const lines = ["C is fun"," Python is cool", "JavaScript is amazing"];
+const lines = ["C is fun","Python is cool", "JavaScript is amazing"];
 let output = "";
 for (let i= 0; i < lines.length; i++) {
     output += lines[i] + (i <lines.length - 1? '\n':'');
