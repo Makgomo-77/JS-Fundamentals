888689c6d5cab89324080cb6bb5d403ea5022c37
Makgomo-77 <102011950+Makgomo-77@users.noreply.github.com>
2025-07-13 20:04:13 +0800

    Update 5-to_integer.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,6 +1,8 @@
 const arg= process.argv[2];
 if (!isNaN(arg) && Number.isInteger(Number(arg))) {
-  console.log(`My number:${Number(arg)}`);
+  console.log(`My number:$ {parseInt(arg)}`);
 }else {
   console.log('Not a number');
 }
+# My number:89
+# My number:-454
