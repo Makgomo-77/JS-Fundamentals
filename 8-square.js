58ef25f80ab373b92255a0c313b8b9da8198ab0e
Makgomo-77 <102011950+Makgomo-77@users.noreply.github.com>
2025-07-20 16:13:07 +0200

    Update 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -3,12 +3,12 @@ const size = parseInt(process.argv[2]);
 
 // Validate input
 if (isNaN(size) || size <= 0) {
-    console.log("Missing size.");
+    console.log("Missing size");
     process.exit(1); // Exit script if input is invalid
 }
 
 // Generate and print the square
 for (let i = 0; i < size; i++) {
-    console.log('x'.repeat(size));
+    console.log('X'.repeat(size));
 }
     
